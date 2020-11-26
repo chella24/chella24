695ba795347debf3cc83749f5332e736c36074db
chella24 <73566109+chella24@users.noreply.github.com>
2020-11-26 09:35:31 +0800

    Add files via upload

---
+++ vowel,consonant.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{ char c;
+    int lowercase_vowel, uppercase_vowel;
+    printf("Enter an alphabet:");
+    scanf("%c",&c);
+
+    if(lowercase_vowel || uppercase_vowel)
+    {
+        printf("%c is a vowel.",c);
+    }
+    else
+    {
+        printf("%c is a consonant.",c);
+    }
+    return 0;
+}
